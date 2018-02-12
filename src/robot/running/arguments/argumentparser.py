22f60e5dc1cc02cf6be473ec2bc8088967a9fbab
Brian Williams <brian.williams@actifio.com>
2018-02-12 16:57:06 -0500

    Add kwonly values to spec and parser
    Get the kwonly values with getfullargspec if py3.

--- src/robot/running/arguments/argumentparser.py
+++ src/robot/running/arguments/argumentparser.py
@@ -20,6 +20,7 @@ if sys.platform.startswith('java'):
     from java.util import List, Map
 
 from robot.errors import DataError
+from robot.utils import PY2
 from robot.variables import is_dict_var, is_list_var, is_scalar_var
 
 from .argumentspec import ArgumentSpec
@@ -31,20 +32,32 @@ class _ArgumentParser(object):
         self._type = type
 
     def parse(self, source, name=None):
-        return ArgumentSpec(name, self._type, *self._get_arg_spec(source))
+        try:
+            return ArgumentSpec(name, self._type, **self._get_arg_spec(source))
+        except TypeError:
+            # Support old style *args return from _get_arg_spec
+            return ArgumentSpec(name, self._type, *self._get_arg_spec(source))
 
     def _get_arg_spec(self, source):
+        """Return a dictionary of kwargs if possible"""
         raise NotImplementedError
 
 
 class PythonArgumentParser(_ArgumentParser):
 
     def _get_arg_spec(self, handler):
-        args, varargs, kwargs, defaults = inspect.getargspec(handler)
+        if PY2:
+            args, varargs, kwargs, defaults = inspect.getargspec(handler)
+            kwonlyargs = kwonlydefaults =  None
+        else:
+            args, varargs, defaults, kwonlyargs, kwonlydefaults, annotations = inspect.getfullargspec(handler)
         if inspect.ismethod(handler) or handler.__name__ == '__init__':
             args = args[1:]  # drop 'self'
         defaults = list(defaults) if defaults else []
-        return args, defaults, varargs, kwargs
+        return {
+            'positional': args, 'defaults': defaults, 'varargs': varargs, 'kwargs': kwargs, 'kwonlyargs': kwonlyargs,
+            'kwonlydefaults': kwonlydefaults
+        }
 
 
 class JavaArgumentParser(_ArgumentParser):
